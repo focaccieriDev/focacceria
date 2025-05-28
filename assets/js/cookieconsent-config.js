78e2b118327ef37529b452b6b92dbbbe95903df5
Zeta90 <davidoff.igle@gmail.com>
2025-05-28 18:38:19 +0200

    perform5 - cookies consent5

--- assets/js/cookieconsent-config.js
+++ assets/js/cookieconsent-config.js
@@ -105,21 +105,18 @@ CookieConsent.run({
         }
     },
     onConsent: (cat) => {
-        console.log(cat)
-        console.log(2)
-        // if (acceptedCategories.includes('analytics')) {
-        //     // Cargar Google Analytics solo si se aceptaron las cookies analíticas
-        //     var gaScript = document.createElement('script');
-        //     gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-WK26E41579";
-        //     gaScript.onload = function () {
-        //         window.dataLayer = window.dataLayer || [];
-        //         function gtag() { dataLayer.push(arguments); }
-        //         gtag('js', new Date());
-        //         gtag('config', 'G-WK26E41579');
-        //         console.log("✅ Google Analytics cargado");
-        //     };
-        //     document.head.appendChild(gaScript);
-        // }
+        if (cat.cookie.categories.includes('analytics')) {
+            var gaScript = document.createElement('script');
+            gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-WK26E41579";
+            gaScript.onload = function () {
+                window.dataLayer = window.dataLayer || [];
+                function gtag() { dataLayer.push(arguments); }
+                gtag('js', new Date());
+                gtag('config', 'G-WK26E41579');
+                console.log("✅ Google Analytics cargado");
+            };
+            document.head.appendChild(gaScript);
+        }
     }
 
 });
