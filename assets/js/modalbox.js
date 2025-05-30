640a468899d7ec71da18b1788a1ead12bdf07a17
Zeta90 <davidoff.igle@gmail.com>
2025-05-30 19:09:15 +0200

    modals

---
+++ assets/js/modalbox.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  /* show lightbox when clicking a thumbnail */
+  $('.fp-info').click(function (event) {
+    event.preventDefault();
+    var content = $('.modal-body');
+    content.empty();
+    // console.log ((this).find('img'))
+    var title = $(this).find('img').attr("data-title");
+      var subtitle = $(this).find('h3 small').html();
+    var description = $(this).find('img').attr("data-description");
+        var image = $(this).find('img').attr("src");
+
+    // console.log(title)
+
+    // content.html($(this).html());
+
+        $('.modal-title').html('<h2>' + title + '</h2><span style="margin-left:10px;font-size:16px">' + subtitle + '</span>');
+        $('.modal-body').html('<img src="' + image + '"/><p style="background-color: #ffffff3b;">' + description + '</p>');
+    // $('.modal-body p').html(description);
+    // console.log(description)  	
+
+
+    $(".modal-profile").modal({ show: true });
+  });
+
+});
